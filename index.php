ff73bd1262cf9c61813559642eebfeb48d8324ac
Yukitaka Maeda <yumaeda@gmail.com>
2018-09-29 17:14:47 +0900

    Create separate JavaScript file for index.php

--- index.php
+++ index.php
@@ -128,27 +128,5 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_name']))
 <script type="text/javascript" src="./app.js"></script>
 <script type="text/javascript" src="./controller/preorder.js?q=1"></script>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script type="text/javascript">
-
-$(document).ready(function()
-{
-    $('div.contents').on('click', 'a.link--action-addtocart', function()
-    {
-        var $parentTd = $(this).closest('td'),
-            strId     = $(this).attr('id'),
-            intQty    = 1;
-
-        $parentTd.html('<img src="../wholesale/load_ajax_post.gif" style="width:15px;height:15px;" />');
-
-        $.post('../cart.php', { pid: strId, action: 'add', qty: intQty, cart_type: 2 })
-            .done(function(data)
-            {
-                $parentTd.html('<img src="../wholesale/success.png" style="width:15px;height:15px;" />');
-            });
-
-        return false;
-    });
-});
-
-</script>
+<script type="text/javascript" src="./index.js"></script>
 
